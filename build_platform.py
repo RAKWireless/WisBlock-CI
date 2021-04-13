9b302b3f6460ddbc3d3f749f6d0cab99b5de8334
beegee1962 <bernd.giesecke@rakwireless.com>
2021-04-13 14:06:32 +0800

    Update platform/board name

--- build_platform.py
+++ build_platform.py
@@ -85,7 +85,8 @@ ALL_PLATFORMS={
     # Adafruit nRF
     "nrf52832" : "adafruit:nrf52:feather52832:softdevice=s132v6,debug=l0",
     "nrf52840" : "adafruit:nrf52:feather52840:softdevice=s140v6,debug=l0",
-    "rak4631" : "raknrf:nrf52:WisCoreRAK4631Board:softdevice=s140v6,debug=l0",
+    "rak4631" : "rakwireless:nrf52:WisCoreRAK4631Board:softdevice=s140v6,debug=l0",
+    "rak11200" : "rakwireless:esp32:WisCoreRAK11200Board:FlashFreq=80",
     "cpb" : "adafruit:nrf52:cplaynrf52840:softdevice=s140v6,debug=l0",
     "clue" : "adafruit:nrf52:cluenrf52840:softdevice=s140v6,debug=l0",
     # groupings
