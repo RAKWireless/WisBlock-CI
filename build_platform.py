03a05458b30900d986aa0391171e4ea4c9d7d3a0
Bernd Giesecke <bernd@giesecke.tk>
2021-05-23 16:27:43 +0800

    Update build_platform.py

--- build_platform.py
+++ build_platform.py
@@ -39,7 +39,7 @@ if "RAK-ESP32-Arduino" in BUILD_DIR:
     IS_ESP_BSP = True
 
 IS_RP2040_BSP = False
-if "RAK-RP2040-Arduino" in BUILD_DIR:
+if "RAK-RP-Arduino" in BUILD_DIR:
     print("Found RP2040 BSP")
     IS_RP2040_BSP = True
 
@@ -97,7 +97,7 @@ ALL_PLATFORMS={
     "nrf52840" : "adafruit:nrf52:feather52840:softdevice=s140v6,debug=l0",
     "rak4631" : "rakwireless:nrf52:WisCoreRAK4631Board:softdevice=s140v6,debug=l0",
     "rak11200" : "rakwireless:esp32:WisCore_RAK11200_Board",
-    "rak11300" : "rakwireless:rp2040:WisCore_RAK11300_Board",
+    "rak11300" : "rakwireless:rp2040:WisBlock_RAK11300_Board",
     "cpb" : "adafruit:nrf52:cplaynrf52840:softdevice=s140v6,debug=l0",
     "clue" : "adafruit:nrf52:cluenrf52840:softdevice=s140v6,debug=l0",
     # groupings
@@ -110,7 +110,7 @@ ALL_PLATFORMS={
 }
 
 # BSP_URLS = "https://downloads.rakwireless.com/Arduino/BSP/package_raknrf_index.json,https://adafruit.github.io/arduino-board-index/package_adafruit_index.json,http://arduino.esp8266.com/stable/package_esp8266com_index.json,https://dl.espressif.com/dl/package_esp32_index.json,https://sandeepmistry.github.io/arduino-nRF5/package_nRF5_boards_index.json"
-BSP_URLS = "https://raw.githubusercontent.com/RAKWireless/RAKwireless-Arduino-BSP-Index/main/package_rakwireless_index.json,https://adafruit.github.io/arduino-board-index/package_adafruit_index.json,http://arduino.esp8266.com/stable/package_esp8266com_index.json,https://dl.espressif.com/dl/package_esp32_index.json,https://sandeepmistry.github.io/arduino-nRF5/package_nRF5_boards_index.json"
+BSP_URLS = "https://raw.githubusercontent.com/RAKWireless/RAKwireless-Arduino-BSP-Index/main/package_rakwireless_index.json,https://raw.githubusercontent.com/RAKWireless/RAK-RP-Arduino/main/package_rakrp_index.json,https://adafruit.github.io/arduino-board-index/package_adafruit_index.json,http://arduino.esp8266.com/stable/package_esp8266com_index.json,https://dl.espressif.com/dl/package_esp32_index.json,https://sandeepmistry.github.io/arduino-nRF5/package_nRF5_boards_index.json"
 
 class ColorPrint:
 
