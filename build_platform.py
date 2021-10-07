20d83a29cf1721c5bbe9631990c8da329906c582
Bernd Giesecke <bernd@giesecke.tk>
2021-10-07 17:33:38 +0800

    Update build_platform.py

--- build_platform.py
+++ build_platform.py
@@ -97,7 +97,7 @@ ALL_PLATFORMS={
     "nrf52840" : "adafruit:nrf52:feather52840:softdevice=s140v6,debug=l0",
     "rak4631" : "rakwireless:nrf52:WisCoreRAK4631Board:softdevice=s140v6,debug=l0",
     "rak11200" : "rakwireless:esp32:WisCore_RAK11200_Board",
-    "rak11300" : "rakrp:mbed_rp2040:WisCoreRAK11300Board",
+    "rak11300" : "rakwireless:mbed_rp2040:WisCoreRAK11300Board",
     "cpb" : "adafruit:nrf52:cplaynrf52840:softdevice=s140v6,debug=l0",
     "clue" : "adafruit:nrf52:cluenrf52840:softdevice=s140v6,debug=l0",
     # groupings
